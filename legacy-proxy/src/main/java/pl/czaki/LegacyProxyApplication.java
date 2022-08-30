54570eaf91caa1ade911fdd4151432a663ea1ab6
Cezary Polosak <Cezary.Polosak@asseco.pl>
2022-08-30 10:40:00 +0200

    initial commit

---
+++ legacy-proxy/src/main/java/pl/czaki/LegacyProxyApplication.java
@@ -0,0 +1,13 @@
+package pl.czaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyProxyApplication.class, args);
+	}
+
+}
